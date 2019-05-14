4b7c935af49f755086929ef09f156d1044dc852a
wangfei <2803548367@qq.com>
2019-05-14 11:37:56 +0800

    d

--- app/src/main/java/com/example/yikangcheng/MainActivity.java
+++ app/src/main/java/com/example/yikangcheng/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.yikangcheng;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.yikangcheng.base.BaseActivtiy;
+
+public class MainActivity extends BaseActivtiy{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initEventData() {
+
+    }
+
+    @Override
+    protected int getActivtiyLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void createPresenter() {
+
     }
 }
